1d47c6fd10987b59ee3dd4e7182751ab6477acde
Hugo Batalha Moreno <hugomb123@gmail.com>
2026-06-20 13:50:44 -0300

    refactor(core): encapsulate global platform bootstrapping routines inside unified app class

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -1,17 +1,19 @@
-import {Bootstrapper} from "./bootstrapper.js"
+import { ItemService } from "../services/item-service.js";
+import { Bootstrapper } from "./bootstrapper.js";
 
 const App = {
-  init() {
-    console.info("AQQ initialized 🚀");
-  }
+  async init() {
+    await Bootstrapper.initialize();
+    if (Date.now() - (localStorage.getItem('aqq_last_check') || 0) > 3600000) {
+    (window.requestIdleCallback || window.setTimeout)(() => {
+        ItemService.processExpiredItems();
+        localStorage.setItem('aqq_last_check', Date.now());
+    });
+}
+
+  },
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  App.init();
-});
-
 document.addEventListener("DOMContentLoaded", async () => {
-
-  await Bootstrapper.initialize();
-
+  await App.init();
 });
